f472b5ac658df9fb372a7e43fcc5df8f4f132f36
IliasToral <118540718+IliasToral@users.noreply.github.com>
2025-12-02 11:33:46 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,16 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #COMM
-
-@app.route("/contact/")
-def contact():
-    return "<h2>Ma page de contact</h2>"
-
-@app.route("/contact/")
-def contact():
-    return "<h2>Ma page de contact</h2>"
-
+    return render_template('hello.html')
   
 if __name__ == "__main__":
   app.run(debug=True)
